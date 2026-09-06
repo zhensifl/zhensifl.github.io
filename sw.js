82461dafa40c855cf0dcd8d5875a3664e7b0ecc1
zhensifl <z18092099792@outlook.com>
2026-09-07 01:00:36 +0800

    Isolate account changes from pending sync and preserve login on network failure

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="wordstep-20260906-1";
-const SHELL=["./","./index.html","./styles.css","./app-20260906-1.js","./enhancements-20260906-1.js","./sync-model-20260906-1.js","./manifest.webmanifest","./icon.svg","./icon-32.png","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png","./version.json"];
+const CACHE_NAME="wordstep-20260907-1";
+const SHELL=["./","./index.html","./styles.css","./app-20260907-1.js","./enhancements-20260907-1.js","./sync-model-20260907-1.js","./manifest.webmanifest","./icon.svg","./icon-32.png","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png","./version.json"];
 const DATA=["./words-data.js","./cefr-levels.js"];
 const NAVIGATION_TIMEOUT=2200;
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(async cache=>{
