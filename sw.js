bce35ed06e0bac1ca1d90ec15ee2ab7e922dd896
zhensifl <z18092099792@outlook.com>
2026-09-07 19:41:16 +0800

    Repair mixed mobile caches and validate vocabulary bundle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="wordstep-20260907-5";
-const SHELL=["./","./index.html","./styles.css","./app-20260907-5.js","./enhancements-20260907-5.js","./sync-model-20260907-5.js","./manifest.webmanifest","./icon.svg","./icon-32.png","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png","./version.json"];
+const CACHE_NAME="wordstep-20260907-6";
+const SHELL=["./","./index.html","./styles.css","./app-20260907-6.js","./enhancements-20260907-6.js","./sync-model-20260907-6.js","./manifest.webmanifest","./icon.svg","./icon-32.png","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png","./version.json"];
 const DATA=["./words-data.js","./cefr-levels.js"];
 const NAVIGATION_TIMEOUT=2200;
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(async cache=>{
